925beb6edb216d170ce1b0341b126d2be9b10e13
abhishek-dev1999 <esthala.abhishek@gmail.com>
2026-01-26 13:58:53 +0530

    config

--- public/config.js
+++ public/config.js
@@ -2,8 +2,8 @@
 // Update these values based on your Keycloak server setup
 
 // Auto-detect current host (works with localhost, LAN IP, and VPN IP)
-// const currentHost = window.location.hostname;
-const currentHost = 'localhost';
+const currentHost = window.location.hostname;
+// const currentHost = 'localhost';
 // const currentHost = '100.122.105.63';//window.location.hostname;
 const protocol = window.location.protocol;
 
@@ -16,7 +16,7 @@ const protocol = window.location.protocol;
 window.config = {
     keycloak: {
         // Use current host for Keycloak URL (works for localhost, LAN, and VPN)
-        url: `http://localhost:8080`,
+        url: `http://100.122.105.63:8080`,
         realm: 'myrealm',
         clientId: 'myclient'
     },
