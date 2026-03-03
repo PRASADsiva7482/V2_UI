53aaa8f1c47bc2894276be222328e1c588d2ea7c
@prasad <v.sivaprasad7482@gmail.com>
2026-03-03 23:53:35 +0530

    browser tab Changes

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,4 +1,6 @@
 import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import TopBar from './TopBar';
 import Navbar from './Navbar';
 import RightSidebar from './RightSidebar';
@@ -6,7 +8,31 @@ import './MainLayout.css';
 
 function MainLayout({ children }) {
     const location = useLocation();
+    const { t } = useTranslation();
     const isSettingsPage = location.pathname.startsWith('/settings');
+
+    useEffect(() => {
+        const path = location.pathname;
+        let title = 'V.com';
+
+        if (path === '/' || path === '/home') {
+            title = `${t('navbar.home', 'Home')} / V.com`;
+        } else if (path.startsWith('/explore')) {
+            title = `${t('navbar.explore', 'Explore')} / V.com`;
+        } else if (path.startsWith('/notifications')) {
+            title = `${t('navbar.notifications', 'Notifications')} / V.com`;
+        } else if (path.startsWith('/connections')) {
+            title = `${t('navbar.connect', 'Connect')} / V.com`;
+        } else if (path.startsWith('/messages') || path.startsWith('/chat')) {
+            title = `${t('navbar.messages', 'Messages')} / V.com`;
+        } else if (path.startsWith('/profile')) {
+            title = `${t('navbar.profile', 'Profile')} / V.com`;
+        } else if (path.startsWith('/settings')) {
+            title = `Settings / V.com`;
+        }
+
+        document.title = title;
+    }, [location.pathname, t]);
 
     return (
         <div className="app-wrapper">
